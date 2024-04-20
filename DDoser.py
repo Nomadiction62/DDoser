05c0e336dff202aed52ce0b132ac36cc29f2a6d5
Nomadiction62 <167362484+Nomadiction62@users.noreply.github.com>
2024-04-20 17:55:45 -0500

    Add files via upload

---
+++ DDoser.py
@@ -0,0 +1,9 @@
+from colorama import Fore, Back, Style
+from queue import Queue
+from optparse import OptionParser
+import time,sys,socket,threading,logging,urllib.request,random
+
+
+
+if __name__ == '__main__':
+	print("\033use Releases\033[0m")
